bdfb3813fe29cca24d9f253b6bde1e214758e8f7
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-01-27 13:40:03 -0800

    first task

--- _print_char.c
+++ _print_char.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * printf_char - prints a char
- * @val: arguments
+ * _print_char - prints a char
+ * @args: arguments
+ *
  * Return: 1
  */
 int _print_char(va_list args)
-{       
-        char c;
+{
+	char c;
 
-        c = va_arg(args, int);
-        _putchar(c);
-        return (1);
+	c = va_arg(args, int);
+	_putchar(c);
+	return (1);
 }
--- _print_string.c
+++ _print_string.c
@@ -8,31 +8,31 @@
  */
 int _strlen(char *s)
 {
-        int i = 0;
+	int i = 0;
 
-        while(s[i] != '\0')
-                i++;
-        return(i);
+	while (s[i] != '\0')
+		i++;
+	return (i);
 }
 
 /**
- * printf_string - print a string
- * @val: arguments
+ * _print_string - print a string
+ * @args: arguments
  *
  * Return: the length of the string
  */
 int _print_string(va_list args)
 {
-        char *s;
-        int i, len;
+	char *s;
+	int i, len;
 
-        s = va_arg(args, char *);
-        if (s == NULL)
-                s = "(nil)";
+	s = va_arg(args, char *);
+	if (s == NULL)
+		s = "(nil)";
 
-        len = _strlen(s);
-        for (i = 0; i < len; i++)
-                _putchar(s[i]);
+	len = _strlen(s);
+	for (i = 0; i < len; i++)
+		_putchar(s[i]);
 
-        return (len);
+	return (len);
 }
