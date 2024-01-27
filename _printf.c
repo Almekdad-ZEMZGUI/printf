62c2f203ddc3e6d9af373f95aa0486c5c64b0118
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-01-27 14:52:33 -0800

    first task

--- _printf.c
+++ _printf.c
@@ -20,27 +20,19 @@ int _printf(const char *format, ...)
 
 	while (format[i] != '\0')
 	{
-		if (format[i] == '%')
+		if (format[i] == '%' && format[i + 1] != '\0')
 		{
-			if (format[i + 1] != '\0')
-                        {
-				if (format[i + 1] == 'c')
-				{
-					charPrinted += _print_char(arg_list);
-				} else if (format[i + 1] == 's')
-				{
-					charPrinted += _print_string(arg_list);
-				} else if (format[i + 1] == '%')
-                        	{
-                                	charPrinted += _putchar('%');
-				}
-				i += 2;
-			} else
+			if (format[i + 1] == 'c')
 			{
-				_putchar('%');;
-				charPrinted++;
-				break;
+				charPrinted += _print_char(arg_list);
+			} else if (format[i + 1] == 's')
+			{
+				charPrinted += _print_string(arg_list);
+			} else if (format[i + 1] == '%')
+			{
+				charPrinted += _putchar('%');
 			}
+			i += 2;
 		} else
 		{
 		_putchar(format[i]);
