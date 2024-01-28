1b91f96a2b3cc6dad7f9ab6cb555bf432f199f6e
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-01-28 08:24:00 -0800

    first task

--- _printf.c
+++ _printf.c
@@ -14,6 +14,8 @@ int _printf(const char *format, ...)
 	va_list arg_list;
 
 	if (format == NULL) return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 
 	va_start(arg_list, format);
 
@@ -40,7 +42,7 @@ int _printf(const char *format, ...)
                         } else
                         {
                                 charPrinted += _printf_percent();
-                                i++;
+                                i++; 
                         }
 
 		} else
