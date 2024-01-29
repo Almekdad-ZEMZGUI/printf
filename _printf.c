b599343b13a08ebb398c65358386b51fcd4dfd69
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-01-29 08:41:27 -0800

    first

--- _printf.c
+++ _printf.c
@@ -9,36 +9,60 @@
 int _printf(const char *format, ...)
 {
         matching arr[] = {
-                {"%c", _print_char}, {"%s", _print_string},
-                {"%%", _print_percent}
+                {'c', _print_char}, {'s', _print_string},
+                {'%', _print_percent}
         };
         int charPrinted = 0;
 	int j, i = 0;
+	int spc;
 
 	va_list arg_list;
         if (format == NULL)
                 return (-1);
 	va_start(arg_list, format);
 
-Here:
 	while(format[i])
         {
-                j = 0;
-                while(j < 3)
-                {
-                        if (format[i] == '%' && !format[i + 1]) return (-1);
-                        if (format[i] == '%' && format[i + 1] == ' ' && !format[i + 2]) return (-1);
-                        if (arr[j].id[0] == format[i] && arr[j].id[1] == format[i + 1])
+                if (format[i] == '%'){
+                        if (!format[i + 1]) return (-1);
+                        if (format[i + 1] == ' ')
                         {
+                                while(format [i + 1] == ' ')
+                                        i++;
+                                spc = 1;
+                                if (format[i + 1] == '\0') return (-1);
+                        }
+                        j = 0;
+                        while (j < 3)
+                        {
+                                if (format[i + 1] == arr[j].id)
+                                {
                                         charPrinted += arr[j].f(arg_list);
                                         i += 2;
-                                        goto Here;
+                                        break;
+                                } else
+                                {
+                                        if (spc)
+                                        {
+                                                charPrinted += _putchar('%');
+                                                charPrinted += _putchar(' ');
+
+                                        }
+                                        charPrinted += _putchar(format[i + 1]);
+                                        i += 2;
+                                        break;
+                                }
+                                j++;
                         }
-                        j++;
+
+                }else
+                {
+                        charPrinted += _putchar(format[i]);
+                        i++;
                 }
-                charPrinted += _putchar(format[i]);
-                i++;
+
         }
+
 	va_end(arg_list);
 	return (charPrinted);
 }
--- main.c
+++ main.c
@@ -10,9 +10,31 @@
 int main(void)
 {
     int len, len2;
-    len = _printf("%%\n");
-    len2 = printf("%%\n");
+
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    len = _printf("%%");
+    len2 = printf("%%");
     printf("%d\n%d", len, len2);
+
+    printf("\n");
+    _printf("Empty string: %s\n", "");
+    _printf("String with spaces: %s\n", "Hello World");
+    _printf("Percent at the end: 42%%\n");
+    _printf("Hello, %s!\n", "world");
+    _printf("Character: %c\n", 'A');
+   _printf("Percentage: %%\n");
+
+
+    _printf("%");
+    _printf("% "); 
+    _printf("%c %"); 
+    _printf("%s %", "hello");
+    
 
     return (0);
 }
