ec0a7816c5448f3717a9b8cf5e857b0d1f620af0
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-01-29 11:46:32 -0800

    break statment

--- main.h
+++ main.h
@@ -4,6 +4,17 @@
 #include <stddef.h>
 #include <unistd.h>
 #include <stdio.h>
+/**
+ * struct matching - matching specifiers with functions
+ * @id: type char of specifier
+ * @f: the function
+ *
+ */
+typedef struct matching
+{
+	char id;
+	int (*f)();
+} matching;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
