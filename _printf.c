a0cffec79e45f32141e0871a32b094726f6644b1
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-01-29 06:45:38 -0800

    first

--- _printf.c
+++ _printf.c
@@ -17,7 +17,10 @@ int _printf(const char *format, ...)
 
 	va_list arg_list;
 
-	if (format == NULL) return (-1);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 
 	va_start(arg_list, format);
 
@@ -27,11 +30,6 @@ Here:
                 j = 0;
                 while(j < 3)
                 {
-                        if (format[i] == '%' && (format[i + 1] == '\0'))
-                                return (-1);
-                        if (format[i] == '%' && format[i + 1] == ' ' && !format[i+2])
-                                return (-1);
-
                         if (arr[j].id[0] == format[i] && arr[j].id[1] == format[i + 1])
                         {
                                         charPrinted += arr[j].f(arg_list);
