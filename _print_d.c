630788db04fdd52df06316c38e928e1c77342a43
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-01-30 05:48:04 -0800

    second task

--- _print_d.c
+++ _print_d.c
@@ -8,79 +8,82 @@
 int _print_i(va_list args)
 {
 	int n = va_arg(args, int);
-	int num, last = n % 10, digit, exp = 1;
-	int  i = 1;
+	int num, lst = n % 10, digit, exp = 1;
+	int count = 1;
 
 	n /= 10;
 	num = n;
 
-	if (last < 0)
+	if (lst < 0)
 	{
 		_putchar('-');
 		num = -num;
 		n = -n;
-		last = -last;
-		i++;
+		lst = -lst;
+		count++;
 	}
-	while (num / 10 != 0)
+	if (num > 0)
 	{
-		exp *= 10;
-		num /= 10;
+		while (num / 10 != 0)
+		{
+			exp *= 10;
+			num /= 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num -= (digit * exp);
+			exp /= 10;
+			count++;
+		}
 	}
-	num = n;
-	while (exp > 0)
-	{
-		digit = num / exp;
-		_putchar(digit + '0');
-		num -= (digit * exp);
-		exp /= 10;
-		i++;
-	}
+	_putchar(lst + '0');
 
-	_putchar(last + '0');
-
-	return (i);
+	return (count);
 }
 
 /**
- * print_d - prints decimal
+ * _print_d - prints decimal
  * @args: argument
  * Return: number of characters printed
  */
 int _print_d(va_list args)
 {
 	int n = va_arg(args, int);
-	int num, last = n % 10, digit, exp = 1;
-	int  i = 1;
+	int num, lst = n % 10, digit, exp = 1;
+	int count = 1;
 
 	n /= 10;
 	num = n;
 
-	if (last < 0)
+	if (lst < 0)
 	{
 		_putchar('-');
 		num = -num;
 		n = -n;
-		last = -last;
-		i++;
+		lst = -lst;
+		count++;
 	}
-	while (num / 10 != 0)
+	if (num > 0)
 	{
-		exp *= 10;
-		num /= 10;
+		while (num / 10 != 0)
+		{
+			exp *= 10;
+			num /= 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num -= (digit * exp);
+			exp /= 10;
+			count++;
+		}
 	}
-	num = n;
-	while (exp > 0)
-	{
-		digit = num / exp;
-		_putchar(digit + '0');
-		num -= (digit * exp);
-		exp /= 10;
-		i++;
-	}
+	_putchar(lst + '0');
 
-	_putchar(last + '0');
-
-	return (i);
+	return (count);
 }
-
