eb20da22e675f48fb2ee68c5c608fe4c1f1fbc35
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-01-30 05:27:55 -0800

    second

--- _print_d.c
+++ _print_d.c
@@ -22,71 +22,33 @@ int _print_i(va_list args)
 		last = -last;
 		i++;
 	}
-	if (num > 0)
+	while (num / 10 != 0)
 	{
-		while (num / 10 != 0)
-		{
-			exp = exp * 10;
-			num = num / 10;
-		}
-		num = n;
-		while (exp > 0)
-		{
-			digit = num / exp;
-			_putchar(digit + '0');
-			num = num - (digit * exp);
-			exp = exp / 10;
-			i++;
-		}
+		exp = exp * 10;
+		num = num / 10;
 	}
+	num = n;
+	while (exp > 0)
+	{
+		digit = num / exp;
+		_putchar(digit + '0');
+		num = num - (digit * exp);
+		exp = exp / 10;
+		i++;
+	}
+
 	_putchar(last + '0');
 
 	return (i);
 }
 
 /**
- * print_d - prints decimal
+ * _print_d - prints decimal
  * @args: argument to print
  * Return: number of characters printed
  */
 
 int _print_d(va_list args)
 {
-	int n = va_arg(args, int);
-	int num, last = n % 10, digit;
-	int  i = 1;
-	int exp = 1;
-
-	n = n / 10;
-	num = n;
-
-	if (last < 0)
-	{
-		_putchar('-');
-		num = -num;
-		n = -n;
-		last = -last;
-		i++;
-	}
-	if (num > 0)
-	{
-		while (num / 10 != 0)
-		{
-			exp = exp * 10;
-			num = num / 10;
-		}
-		num = n;
-		while (exp > 0)
-		{
-			digit = num / exp;
-			_putchar(digit + '0');
-			num = num - (digit * exp);
-			exp = exp / 10;
-			i++;
-		}
-	}
-	_putchar(last + '0');
-
-	return (i);
+	return (_print_i(args));
 }
-
