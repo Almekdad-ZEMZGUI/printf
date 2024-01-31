d4eb578fdafe189af3fee025250ea4b398f01f60
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-01-31 04:45:03 -0800

    test commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main() {
+    _printf("%R\n", "Hello, World!");
+    return 0;
+}
