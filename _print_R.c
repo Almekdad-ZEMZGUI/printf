551b0e6a1b41feadf8286322c12640f1042d2468
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-01-30 17:03:43 -0800

    R

--- _print_R.c
+++ _print_R.c
@@ -8,32 +8,26 @@
  */
 int _print_rot13(va_list args)
 {
-	int i, j, count = 0;
-	int k = 0;
-	char *s = va_arg(args, char*);
-	char arr[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-	char rot[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+    char *s = va_arg(args, char*);
+    int i, count = 0;
 
-	if (s == NULL)
-		s = "(null)";
-	for (i = 0; s[i]; i++)
-	{
-		k = 0;
-		for (j = 0; arr[j] && !k; j++)
-		{
-			if (s[i] == arr[j])
-			{
-				_putchar(rot[j]);
-				count++;
-				k = 1;
-			}
-		}
-		if (!k)
-		{
-			_putchar(s[i]);
-			count++;
-		}
-	}
-	return (count);
+    if (s == NULL)
+        s = "(null)";
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+        {
+            char base = (s[i] >= 'a' && s[i] <= 'z') ? 'a' : 'A';
+            _putchar((s[i] - base + 13) % 26 + base);
+            count++;
+        }
+        else
+        {
+            _putchar(s[i]);
+            count++;
+        }
+    }
+
+    return (count);
 }
-
